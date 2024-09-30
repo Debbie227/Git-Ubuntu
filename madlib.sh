9baa9c8aff75b6838d51d45a91860f24fdd8c1dd
Debbie227 <110264572+Debbie227@users.noreply.github.com>
2024-09-30 00:21:15 +0000

    Shell script madness for class

---
+++ madlib.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+#It's time to game! Fill in the parts of speech to create a wacky story.
+
+echo "Please play a fun fall MadLib with me!"
+
+sleep 2
+
+echo "First enter an adjective. (desriptive word)"
+
+read adj1
+
+sleep 1
+
+echo "Please enter a color."
+
+read color1
+
+sleep 1
+
+echo "Please enter a vegetable."
+
+read veg1
+
+sleep 1
+
+echo "Now enter a noun. (person/place/thing)"
+
+read noun1
+
+sleep 1
+
+echo "Please enter a verb. (action word)"
+
+read verb1
+
+sleep 1
+
+echo "Have you remembered all the parts of speech?"
+
+sleep 2
+
+echo "Now enter another vegetable."
+
+read veg2
+
+echo "Please enter another adjective."
+
+read adj2
+
+sleep 1
+
+echo "Please enter a noun."
+
+read noun2
+
+sleep 2
+
+echo "Please enter a verb."
+
+read verb2
+
+sleep 1
+
+echo "We're almost done!"
+
+sleep 2
+
+echo "Please enter yet another vegetable."
+
+read veg3
+
+sleep 1
+
+echo "And finally please enter one more noun"
+
+read noun3
+
+sleep 2
+
+echo "What a silly story!"
+
+sleep 2
+
+echo ""
+
+echo "It's a $adj1 fall day, the sky is $color1, and we're going to the $veg1 patch!"
+
+sleep 1
+
+echo "They have $noun1 rides there where you can $verb1 your own $veg2."
+
+sleep 1
+
+echo "There's also a $adj2 house where scary ${noun2}s jump out to try and scare you."
+
+sleep 1
+
+echo "I like ${verb2}ing my way through the $veg3 maze, where the ${noun3}s are taller than the adults."
